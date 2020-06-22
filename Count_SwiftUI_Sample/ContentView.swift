48a548b9a5f995f5436df751bc1ae0d0570e5741
tosh7 <zlia.6.lj.425@gmail.com>
2020-06-23 08:26:24 +0900

    initial commit

---
+++ Count_SwiftUI_Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Count_SwiftUI_Sample
+//
+//  Created by satoshi.komatsu on 2020/06/23.
+//  Copyright © 2020 tosh7. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
